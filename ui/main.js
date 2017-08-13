986c61320989b18032f063ba558c3fbc57fd609e
ParuchuriMaruthi <30751214+ParuchuriMaruthi@users.noreply.github.com>
2017-08-13 14:04:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ element.innerHTML='New Value';
 //move image
 var img=document.getElementById('madi');
 img.onclick=function(){
-    img.style.marginleft='100px';
+    var interval=setInterval(moveLeft,100);
     
 };
