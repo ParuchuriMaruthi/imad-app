5c1f300cbee324717c905c2272f795f484eab27c
ParuchuriMaruthi <30751214+ParuchuriMaruthi@users.noreply.github.com>
2017-08-13 11:17:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+//move image
+var img=document.getElementById('maru');
+maru.onclick=function(){
+    maru.style.marginLeft='100px';
+    
+};
